86907b91a9296e14d01929882cacd27846c74e5d
Gary Snider <75227981+gsnider2195@users.noreply.github.com>
2024-06-25 14:35:37 -0500

    add validation error test

--- designs/initial_data/context/__init__.py
+++ designs/initial_data/context/__init__.py
@@ -1,7 +1,11 @@
-from nautobot_design_builder.context import Context
+from nautobot_design_builder.context import Context, DesignValidationError
 
 
 class InitialDesignContext(Context):
     """Render context for basic design"""
 
     routers_per_site: int
+
+    def validate_something(self):
+        # testing, raise DesignValidationError
+        raise DesignValidationError("test failure")
