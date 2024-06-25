b2e24b4542086c0e4b6b2bf3bacf3762ea7b718f
Gary Snider <75227981+gsnider2195@users.noreply.github.com>
2024-06-25 16:27:28 -0500

    fix

--- designs/jobs.py
+++ designs/jobs.py
@@ -1,10 +1,8 @@
 """Import designs so they are discoverable by `load_jobs`."""
 
-from .initial_data.jobs import InitialDesign
 from .core_site.jobs import CoreSiteDesign
 
 
 __all__ = (
-    "InitialDesign",
     "CoreSiteDesign",
 )
