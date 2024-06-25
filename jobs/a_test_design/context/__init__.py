d75dd0d00fd9b99ce48ba6afd61c025e638753e1
Gary Snider <75227981+gsnider2195@users.noreply.github.com>
2024-06-25 16:46:37 -0500

    test

--- jobs/a_test_design/context/__init__.py
+++ jobs/a_test_design/context/__init__.py
@@ -1,5 +1,8 @@
+import logging
 from nautobot_design_builder.context import Context, DesignValidationError
 
+
+logger = logging.getLogger(__name__)
 
 class InitialDesignContext(Context):
     """Render context for basic design"""
@@ -8,5 +11,6 @@ class InitialDesignContext(Context):
 
     def validate_something(self):
         # testing, raise DesignValidationError
+        logger.debug("RUNNING VALIDATION")
         raise DesignValidationError("test failure")
         pass
